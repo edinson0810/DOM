98f6adc75e50c44b5bad213e7d342cdce773b192
Nicolas Rodriguez <pufiavila@gmai.com>
2025-02-21 21:31:10 -0500

    nuevo

---
+++ nuevo/app.js
@@ -0,0 +1,19 @@
+const input = document.querySelector(`input`)
+const consena = document.querySelector(`input[type="password]`);
+console.log(input);
+console.log(consena);
+console.log(input.type);
+console.log(input.value);
+console.log(input.placeholder);
+input.valu = "Edinson barrios";
+console.log(consena.value ? ` lleno` : `vacio`);
+consena.value = "11111";
+
+consena.setAttribute(`type`, `text`);
+
+
+
+
+
+
+
