71181dc67057f5bb88bf47c249e91c78e78afa2c
edinson barrios <lache081015@gmail.com>
2025-02-26 10:59:35 -0500

    agregado el selec con las ciudades

--- atributos/app.js
+++ atributos/app.js
@@ -13,58 +13,69 @@ const ciudades = async() => {
     const ciudades = await data.json();
     console.log(ciudades);
   
-    ciudades.
+  const selectCiudad = document.createElement("select")
+  ciudades.forEach(ciudad => {
+    const opcion = document.createElement("option");
+    opcion.value = ciudades.nombre;
+    opcion.textContent = ciudad.nombre;
+    selectCiudad.append(opcion)
+  });
+
+
+
+  formulario.append(selectCiudad)
 }
+
 ciudades();
 
 
 
 
 
-const validar = (event) => {
-    // detenemos el evento
-    event.preventDefault()
-if (nombre.value != ``) {
-    alert("el nombre es obligatotia");
-    nombre.focus()
-}
-}
+// const validar = (event) => {
+//     // detenemos el evento
+//     event.preventDefault()
+// if (nombre.value != ``) {
+//     alert("el nombre es obligatotia");
+//     nombre.focus()
+// }
+// }
 
-// le pongo el evento click
-btn.addEventListener(`click`, validar)
-// le quito el evento click
-btn.removeEventListener(`click`, validar)
-
-
-// capturar el click derecho en el formulario
-const contextmenu = () => {
-alert(`?`)
-}
-body.addEventListener(`contextmenu`, contextmenu )
-// formulario.addEventListener(`contextmenu`, contextmenu )
+// // le pongo el evento click
+// btn.addEventListener(`click`, validar)
+// // le quito el evento click
+// btn.removeEventListener(`click`, validar)
 
 
-// capturar doble click sale el mensaje
-const dblclick = () => {
-    alert(`doble click`)
-}
-body.addEventListener(`dblclick`, dblclick)
+// // capturar el click derecho en el formulario
+// const contextmenu = () => {
+// alert(`?`)
+// }
+// body.addEventListener(`contextmenu`, contextmenu )
+// // formulario.addEventListener(`contextmenu`, contextmenu )
 
 
-// captura cualquier click y se muestra la alerta
-const mousedown = () => {
-    alert(` el evento funciona cuando se presiona cualquier boton sobre el elemento`)
-}
-body.addEventListener(`mousedown`,mousedown)
+// // capturar doble click sale el mensaje
+// const dblclick = () => {
+//     alert(`doble click`)
+// }
+// body.addEventListener(`dblclick`, dblclick)
 
-// presionaste la tecla 
-const keydown = (event) => {
-    console.log(`presionaste la tecla ${event.key}`)
-    console.log(`presionaste ${event.keypress}`);
-    console.log(`presionaste ${event.keyup}`);
+
+// // captura cualquier click y se muestra la alerta
+// const mousedown = () => {
+//     alert(` el evento funciona cuando se presiona cualquier boton sobre el elemento`)
+// }
+// body.addEventListener(`mousedown`,mousedown)
+
+// // presionaste la tecla 
+// const keydown = (event) => {
+//     console.log(`presionaste la tecla ${event.key}`)
+//     console.log(`presionaste ${event.keypress}`);
+//     console.log(`presionaste ${event.keyup}`);
     
-}
-nombre.addEventListener(`keydown`, keydown)
+// }
+// nombre.addEventListener(`keydown`, keydown)
 
 
 
