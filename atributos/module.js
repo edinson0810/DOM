6706f2b3393f504461599be3ae1ff6f43a82fa9d
edinson barrios <lache081015@gmail.com>
2025-03-02 12:15:36 -0500

    falta el div masculino

--- atributos/module.js
+++ atributos/module.js
@@ -4,31 +4,51 @@
 console.log(Generos);
 
 
+const div = document.createElement("div")
+div.classList.add("div1")
+const label = document.createElement("label")
+label.textContent = "femino"
+label.setAttribute("for", "radio1")
 
+const radio = document.createElement("input")
+radio.type = "radio"
+radio.id = "radio1"
+radio.name = "opciones";
 
-const newLabel = document.createElement('label');
-newLabel.innerHTML ="Femenino";
-//agregando el label
-var contenedor = document.getElementById('formulario');
-contenedor.append(newLabel);
+div.appendChild(label)
+div.appendChild(radio)
 
-
-const Femenino = document.createElement("input")
-Femenino.setAttribute("type", "radio");
-
-const newLabel1 = document.createElement('label');
-newLabel1.innerHTML ="Masculino";
-//agregando el label
-var contenedor = document.getElementById('formulario');
-contenedor.append(newLabel1);
-
-
-const masculino = document.createElement("input")
-masculino.setAttribute("type", "radio")
+formulario.appendChild(div)
 
 
 
-  formulario.append(newLabel,Femenino,newLabel1,masculino)
+
+
+
+
+// const newLabel = document.createElement('label');
+// newLabel.innerHTML ="Femenino";
+// //agregando el label
+// var contenedor = document.getElementById('formulario');
+// contenedor.append(newLabel);
+
+
+// const Femenino = document.createElement("input")
+// Femenino.setAttribute("type", "radio");
+
+// const newLabel1 = document.createElement('label');
+// newLabel1.innerHTML ="Masculino";
+// //agregando el label
+// var contenedor = document.getElementById('formulario');
+// contenedor.append(newLabel1);
+
+
+// const masculino = document.createElement("input")
+// masculino.setAttribute("type", "radio")
+
+
+
+//   formulario.append(newLabel,Femenino,newLabel1,masculino)
 
 
 
